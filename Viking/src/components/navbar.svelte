af2d605738ab2ef988de3a022e4eecda219eae26
Daniel Johan Sørby <81819141+loliskrem@users.noreply.github.com>
2024-10-08 10:40:56 +0200

    Initial commit

---
+++ Viking/src/components/navbar.svelte
@@ -0,0 +1,39 @@
+<script>
+    // your script goes here
+</script>
+
+<div class="navbar">
+    <div class="logo-container">
+        <img src="" alt="Logo">
+    </div>
+    <ul class="nav-links">
+        <a href="/"><li>Home</li></a>
+        <a href="/"><li>About</li></a>
+        <a href="/"><li>Home</li></a>
+        <a href="/"><li>Home</li></a>
+    </ul>
+</div>
+
+<style>
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 20px;
+    }
+
+    .nav-links {
+        display: flex;
+        justify-content: space-evenly;
+    }
+
+    .nav-links a {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    .nav-links li {
+        list-style: none;
+        margin: 0 10px;
+    }
+</style>
